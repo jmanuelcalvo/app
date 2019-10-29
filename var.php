1cf11d222b0fdd686f5d12c1f291db34ff2c409f
Jose Manuel Calvo I <jmanuelcalvo@gmail.com>
2019-10-28 19:29:38 -0500

    Update var.php

--- var.php
+++ var.php
@@ -1,4 +1,7 @@
 <?php
-  $db = getenv('APP_MSG'); // Gets the so var name
-  echo "El nombre seteado en el configMap es: $db  \r\n"
+  $titulo = getenv('APP_HEADER'); // Gets the so var name
+  $msg = getenv('APP_MSG'); // Gets the so var name
+
+  echo "<h4>Bienvenidos a: $titulo  \r\n</h4><br>";
+  echo "<p>El valor del CM/APP_MSG es: $msg  \r\n</p>"
 ?>
