8dc49b5b2364af2614cccc469bc0d78697049082
Jose Manuel Calvo I <jcalvo@redhat.com>
2022-11-16 12:15:53 -0600

    Cambios en el codigo

--- hostname.php
+++ hostname.php
@@ -1,3 +1,3 @@
 <?php
-  print "Esta es la pagina de la app1. Se ejecuta en el host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
+  print "Esta es la pagina de la app. Se ejecuta en el host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
 ?>  
